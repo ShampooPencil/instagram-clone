c40a68ba9e009cb43962871486d00347e23c956f
Andrew Aramouni <aramouni8@gmail.com>
2023-10-07 21:23:16 -0400

    starting the project

---
+++ instagram-clone/src/Header.js
@@ -0,0 +1,19 @@
+    // src/components/Header/index.js
+  
+    
+    export default function Header(){
+      
+            return (
+               <nav className="Nav">
+                 <div className="Nav-menus">
+                   <div className="Nav-brand">
+                     <a className="Nav-brand-logo" href="/">
+                       Instagram
+                     </a>
+                   </div>
+                 </div>
+               </nav>
+            )
+            
+    }
+ 
